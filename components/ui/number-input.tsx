6f11b517ea3babbf6834b5c8a6537a5452145b21
MrAHMED <chikhaoui084@gmail.com>
2025-01-29 18:05:21 -0800

    feat: create number input component and inputs form with validation

---
+++ components/ui/number-input.tsx
@@ -0,0 +1,52 @@
+import { Input } from "@/components/ui/input"
+import { cn } from "@/lib/utils"
+import { ControllerRenderProps, FieldValues, Path } from "react-hook-form"
+
+interface NumberInputProps<
+  TFieldValues extends FieldValues,
+  TName extends Path<TFieldValues>
+> {
+  field: ControllerRenderProps<TFieldValues, TName>
+  className?: string
+  placeholder?: string
+  pattern?: string
+  disabled?: boolean
+  inputMode?:
+    | "search"
+    | "text"
+    | "email"
+    | "tel"
+    | "url"
+    | "numeric"
+    | "none"
+    | "decimal"
+}
+
+export default function NumberInput<
+  TFieldValues extends FieldValues,
+  TName extends Path<TFieldValues>
+>({
+  field,
+  placeholder,
+  className,
+  inputMode,
+  pattern,
+  disabled,
+}: NumberInputProps<TFieldValues, TName>) {
+  return (
+    <Input
+      type="number"
+      inputMode={inputMode ?? "numeric"}
+      placeholder={placeholder}
+      pattern={pattern}
+      disabled={disabled}
+      className={cn("w-full", className)}
+      {...field}
+      value={field.value ?? ""}
+      onChange={(e) => {
+        if (e.target.value === "") return field.onChange(undefined)
+        field.onChange(Number(e.target.value))
+      }}
+    />
+  )
+}
