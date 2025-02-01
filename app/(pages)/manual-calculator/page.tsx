9fb60b0ddeb490748fe4e268af2e50c788915e12
MrAHMED <chikhaoui084@gmail.com>
2025-02-01 09:35:28 -0800

    feat: add ManualCalculatorPage component to render ManualCalculator

---
+++ app/(pages)/manual-calculator/page.tsx
@@ -0,0 +1,9 @@
+import ManualCalculator from "@/components/global/manual-calculator/manual-calculator"
+
+export default function ManualCalculatorPage() {
+  return (
+    <div className="w-full mt-10">
+      <ManualCalculator />
+    </div>
+  )
+}
