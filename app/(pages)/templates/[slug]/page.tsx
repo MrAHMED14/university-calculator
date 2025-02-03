9888017c834d8acbc0a6357262ed0c2ce3f06436
MrAHMED <chikhaoui084@gmail.com>
2025-02-03 13:46:51 -0800

    feat: integrate initializeValues function for dynamic data handling in GenericCalculator

--- app/(pages)/templates/[slug]/page.tsx
+++ app/(pages)/templates/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import GenericCalculator from "@/components/global/generic-calculator/generic-calculator"
 import MaxWidthWrapper from "@/components/global/max-width-wrapper"
 import { INFO_L3_ISIL } from "@/lib/static-config"
+import { initializeValues } from "@/lib/utils"
 import { notFound } from "next/navigation"
 
 export default async function Page({
@@ -9,6 +10,7 @@ export default async function Page({
   params: Promise<{ slug: string }>
 }) {
   const slug = (await params).slug
+
   if (slug !== "info-l3-isil") {
     notFound()
   }
@@ -17,11 +19,13 @@ export default async function Page({
     notFound()
   }
 
+  const data = initializeValues(INFO_L3_ISIL)
+
   return (
     <MaxWidthWrapper className="my-20 w-full max-w-6xl">
       <h1 className="text-3xl font-semibold">Computer science department</h1>
       <p className="text-muted-foreground">L3 ISIL specialization</p>
-      <GenericCalculator data={INFO_L3_ISIL} />
+      <GenericCalculator data={data} />
     </MaxWidthWrapper>
   )
 }
