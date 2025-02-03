1b62042802f5009e9d8c870d67d6e0ed850acf8e
MrAHMED <chikhaoui084@gmail.com>
2025-02-03 13:47:12 -0800

    feat: add reset button to Semaster cards in GenericCalculator

--- components/global/generic-calculator/generic-calculator.tsx
+++ components/global/generic-calculator/generic-calculator.tsx
@@ -5,6 +5,9 @@ import React, { useState } from "react"
 import GenericSemasterTable from "./generic-semaster-table"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import ManualYearTable from "../manual-calculator/manual-year-table"
+import { Button } from "@/components/ui/button"
+import { resetValue } from "@/lib/utils"
+import { XIcon } from "lucide-react"
 
 export default function GenericCalculator({
   data,
@@ -18,7 +21,17 @@ export default function GenericCalculator({
     <div className="mt-10 space-y-10">
       <Card className="w-full border border-gray-200 shadow-sm overflow-hidden max-w-6xl">
         <CardHeader>
-          <CardTitle>Semaster 1</CardTitle>
+          <CardTitle>
+            <div className="w-full flex items-center justify-between">
+              <span>Semaster 1</span>
+              <Button
+                size="icon"
+                onClick={() => setSemaster1(resetValue(semaster1))}
+              >
+                <XIcon className="size-4" />
+              </Button>
+            </div>
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <GenericSemasterTable
@@ -30,7 +43,17 @@ export default function GenericCalculator({
 
       <Card className="w-full border border-gray-200 shadow-sm overflow-hidden max-w-6xl">
         <CardHeader>
-          <CardTitle>Semaster 2</CardTitle>
+          <CardTitle>
+            <div className="w-full flex items-center justify-between">
+              <span>Semaster 2</span>
+              <Button
+                size="icon"
+                onClick={() => setSemaster2(resetValue(semaster2))}
+              >
+                <XIcon className="size-4" />
+              </Button>
+            </div>
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <GenericSemasterTable
